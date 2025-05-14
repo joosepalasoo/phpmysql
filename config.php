e221bd18d9c54ca9f77976973fdc604cdcac7cbb
joosepalasoo <joosep.alasoo@hkhk.edu.ee>
2025-05-14 13:50:54 +0300

    login and shit

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
 	try {
-		// sinu andmed
+
 		$db_server = 'localhost';
 		$db_andmebaas = 'sport2025';
 		$db_kasutaja = 'joosep';
-		$db_salasona = 'joosep';
+		$db_salasona = 'admin';
 
-		// ühendus
+		
 		$yhendus = mysqli_connect($db_server, $db_kasutaja, $db_salasona, $db_andmebaas);
 	} catch (mysqli_sql_exception $e) {
 		die('Probleem andmebaasiga: ' . $e->getMessage());
---
+++ login.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>admin leht</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+  </head>
+  <body>
+  <?php
+	session_start();
+	if (isset($_SESSION['tuvastamine'])) {
+	  header('Location: admin.php');
+	  exit();
+	  }
+	if (!empty($_POST['login']) && !empty($_POST['pass'])) {
+		$login = $_POST['login'];
+		$pass = $_POST['pass'];
+		if ($login=='admin' && $pass=='admin') {
+			$_SESSION['tuvastamine'] = 'misiganes';
+			header('Location: admin.php');
+		}
+	}
+?>
+<h1>Login</h1>
+<form action="" method="post">
+	Login: <input type="text" name="login"><br>
+	Password: <input type="password" name="pass"><br>
+	<input type="submit" value="Logi sisse">
+</form>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+  </body>
+</html>
